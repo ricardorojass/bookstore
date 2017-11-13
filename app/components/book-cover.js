62b4ecaadde49933525de2204d947af90b59ebe3
Ricardo Rojas <reddrum7@gmail.com>
2017-11-13 18:00:40 -0500

    Add modal

--- app/components/book-cover.js
+++ app/components/book-cover.js
@@ -1,4 +1,20 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+
+	actions: {
+
+    open() {
+      this.get('book').reload().then(() => {
+        this.set('isShowingModal', true);
+        this.get('blurBackground')(true);
+      });
+    },
+
+    close() {
+      this.set('isShowingModal', false);
+      this.get('blurBackground')(false);
+    }
+
+  }
 });
