320ab2212b057c2f0147166d0acc7ddb665369e3
Ricardo Rojas <reddrum7@gmail.com>
2017-11-13 18:56:17 -0500

    Finish part 2

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,13 +4,30 @@
 // });
 import Ember from 'ember';
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
+import { pluralize } from 'ember-inflector';
 
-const { String: { pluralize, underscore } } = Ember;
+const { String: { underscore } } = Ember;
 
 export default JSONAPIAdapter.extend({
 
   pathForType(type) {
     return pluralize(underscore(type));
-  }
+  },
 
+  shouldReloadRecord() {
+    return false;
+  },
+
+  shouldBackgroundReloadRecord(store, snapshot) {
+
+    console.log("Calling shouldBackgroundReloadRecord");
+    const loadedAt = snapshot.record.get('loadedAt');
+
+    // if it was loaded more than an hour ago
+    if (Date.now() - loadedAt > 3600000) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 });
